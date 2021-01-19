7bfa2cae66fe4441a25050be6766691ff71a92f1
ps9610 <ps961001@gamil.com>
2021-01-19 17:48:15 +0900

    19일 슬라이드까지 진도완료, vue.js 시작

---
+++ 01_19/join.php
@@ -0,0 +1,153 @@
+<?
+    include_once("header.php")
+?>
+
+<main id="join">
+
+    <section id="section1">
+        <div class="wrap">
+            <div class="gap">
+                <div class="container">
+                    <div class="title">
+                        <h2>Join</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="section2">
+        <div class="wrap">
+            <div class="gap">
+                <div class="container">
+                    <div class="content">
+                        <ul class="clearfix">
+                            <li>
+                                <div class="content-gap">
+                                    <div class="title">
+                                        <h3>Join</h3>
+                                        <h2>회원가입</h2>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content-gap">
+                                    <h2>개인정보 수집동의</h2>
+                                    <p>(주)제이슨여행사(이하 “회사”)는 귀하의 개인정보 보호를 위해 중요시하며, ‘개인정보보호법’등 관련 법규를 준수하기 위하여 개인정보처리방침을 제정하고 이를 준수하고 있습니다.
+                                        개인정보처리방침을 통하여 귀하께서 제공하시는 개인정보가 어떠한 용도와 방식으로 이용되고 있으며 정보보호를 위해 어떠한 조치가 취해지고 있는지를 알려 드립니다.
+                                        귀하께서 제공해주신 모든 정보는 관계 법령에 의거 철저히 관리되고 있으며 본 개인정보처리방침은 관련 법률 및 지침의 변경과 회사 내부 운영방침의 변경에 따라 변경될 수 있습니다.
+                                        변경사항은 본 사이트에서 확인하실 수 있습니다.
+                                        <br>
+                                        1. 개인정보 수집 항목
+
+                                        회사는 창업상담, 고객의 소리 등을 위해 아래와 같은 개인정보를 수집하고 있습니다.
+
+                                        • 재화 또는 서비스 제공
+                                        - 필수 항목: 이름, 전화번호(휴대폰번호), 이메일 주소, 내용
+                                        - 선택 항목: 희망지역, 알게된 경로, 이용매장, 상담내용, 기타
+
+                                        2. 개인 정보의 수집·이용 목적
+
+                                        회사가 개인정보를 수집하는 기본적인 목적은 상담, 문의, 수강 등 회사의 서비스 이용을 위한 것이며 수집된 정보를 바탕으로 보다 나은 서비스 혜택 제공을 위해 다양한 방법(이메일, SMS, 전화 등)을 통하여 서비스 관련 정보를 제공하는데 있습니다. 수집된 개인정보는 다음의 목적 이외의 용도로는 이용되지 않으며 이용 목적이 변경될 시에는 개인정보보호법 관련 법령에 따라 사전 동의를 구할 예정입니다.
+
+                                        • 재화 또는 서비스 제공
+                                        서비스 제공, 콘텐츠 제공, 맞춤 서비스 제공 등을 목적으로 개인정보를 처리합니다.
+                                        • 마케팅 및 광고에의 활용
+                                        신규 서비스(제품) 개발 및 맞춤 서비스 제공, 이벤트 및 광고성 정보 제공 및 참여기회 제공 등을 목적으로 개인정보를 처리합니다.
+
+                                        3. 개인 정보의 보유 및 이용 기간
+
+                                        회사는 법령에 따른 개인정보 보유·이용기간 또는 정보주체로부터 개인정보를 수집시에 동의 받은 개인정보 보유·이용기간 내에서 개인정보를 처리·보유합니다.
+                                        개인정보 처리 및 보유기간은 다음과 같습니다.
+
+                                        • 재화 또는 서비스 제공
+                                        재화·서비스 공급완료 및 요금결제·정산 완료시까지
+                                        단, 관계법령의 규정에 의하여 보존할 필요가 있는 경우 회사는 아래와 같이 관계법령에서 정한 일정한 기간 동안 개인정보를 보관합니다. 
+                                        - 계약 또는 청약철회 등에 관한 기록: 5년 (전자상거래등에서의 소비자보호에 관한 법률) 
+                                        - 대금결제 및 재화 등의 공급에 관한 기록: 5년 (전자상거래등에서의 소비자보호에 관한 법률) 
+                                        - 소비자의 불만 또는 분쟁처리에 관한 기록: 3년 (전자상거래등에서의 소비자보호에 관한 법률)
+
+                                        4. 개인정보 수집 및 이용 동의를 거부할 권리
+
+                                        귀하는 개인정보의 수집 및 이용에 대하여 동의하지 않을 권리가 있습니다. 단, 회사에서 수집하는 개인정보는 서비스 제공에 있어 필수적인 정보로써 개인정보의 수집 및 이용에 동의하지 않는 이용자는 서비스 이용이 제한될 수 있습니다.
+                                    </p>
+                                    <div>
+                                        <input type="checkbox" name="agree" id="agree" valve="" >
+                                        <a href="#" title="agreebox"><label for="agree">개인정보 수집에 동의합니다.</label>(필수)</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content-gap">
+                                    <h2>마케팅 수신 동의</h2>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <th>목적</th>
+                                                <th>수집항목</th>
+                                                <th>보유기간</th>
+                                            </tr>
+                                            <tr>
+                                                <td>이벤트, 혜택 제공 및 고객별 맞춤형 여행관련상품/서비스 제공</td>
+                                                <td>휴대전화, 이메일계정 주소</td>
+                                                <td>서비스 제공 및 관계법령에 따른 보존기간</td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="3">고객님께서는 광고성 정보수신 동의를 거부할 권리가 있습니다. 이경우, 여행서비스 이용에는 영향이 없습니다.</td>
+                                            </tr>
+                                        </tbody>    
+                                    </table>
+                                    <div>
+                                        <input type="checkbox" name="agree" id="agree" valve="" >
+                                        <a href="#" title="agreebox"><label for="agree">마케팅 수신에 동의합니다.</label>(선택)</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content-gap">
+                                    <h2>정보입력</h2>
+                                    <div>
+                                        <label for="email">이메일(ID)<span> *</span></label>
+                                        <input type="email" id="email" name="email" placeholder="이메일 형식 sample@sample.com"></div>
+                                    <div>
+                                        <label for="yourName">이름<span> *</span></label>
+                                        <input type="text" id="yourName" name="yourName" placeholder="이름을 입력하세요."></div>
+                                    <div>
+                                        <label for="yourPW">비밀번호<span> *</span></label>
+                                        <input type="password" id="yourPW" name="yourPW" placeholder="영문, 숫자, 특수문자(!@#$%^&*) 조합 8~12"></div>
+                                    <div>
+                                        <label for="againPW">비밀번호 확인<span> *</span></label>
+                                        <input type="password" id="againPW" name="againPW" placeholder="비밀번호를 한번 더 입력하세요"></div>
+                                    <div>
+                                    <label for="yourNum">휴대폰번호<span> *</span></label>
+                                    <input type="password" id="yourNum" name="yourNum"></div>
+                                    <p><span>* </span>필수입력항목입니다.</p>
+                                    <div>
+                                        <label for="text">주소</label>
+                                        <input type="text" id="address" name="address">
+                                        <input type="submit" id="findAddress" name="findAddress" value="우편번호찾기">
+                                    </div>
+                                    <div>
+                                        <label for="email">상세주소</label>
+                                        <input type="text" id="detailAdd" name="detailAdd"></div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content-gap">
+                                    <div>
+                                        <input type="submit" id="joinUs" name="joinUs" value="회원가입">
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?
+    include_once("footer.php")
+?>
