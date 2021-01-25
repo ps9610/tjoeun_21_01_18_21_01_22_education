83ff8d5faeaeae69ffc2fd70ed963434b6426093
ps9610 <ps961001@gamil.com>
2021-01-25 09:28:40 +0900

    서버 php만들기

--- 01_21/select.php
+++ 01_21/select.php
@@ -5,10 +5,10 @@
     header("Content-type:text/html; charset=utf8"); 
 
     //데이터베이스 서버의 접속 정보 변수 등록
-    $servername = localhost;
-    $username   = sohye;
-    $password   = qkthgp6438;
-    $dbname     = sohye; 
+    $servername = "localhost";
+    $username   = "sohye";
+    $password   = "qkthgp6438!";
+    $dbname     = "sohye"; 
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     mysqli_set_charset($conn, "utf8");   //데이터베이스 인코딩 = 문자 세팅을 해라라는 뜻
@@ -27,11 +27,10 @@
     //변수 = 정보를 검색해라 , 모든 것을, login_table(리눅스 환경에서는 웬만하면 소문자, 공백없이) 로부터
     $sql = "select * from login_table";
     //데이터베이스 접속
-    $result = mysql_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
     
     //이 에코를 AJAX를 이용해서 이동할거임. 정보 검색 본문. 반복문 프로그래밍
     echo $result;
-    //이 에코를 AJAX를 이용해서 이동할거임. 정보 검색 본문. 반복문 프로그래밍
 
     /////////////////////////////////////////////////본문 끝, 나머지는 푸터/////////////////////////////////////////////////
     // 정보 검색이 끝났다면 데이터베이스 닫아주기
